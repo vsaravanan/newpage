4d187172f2ff931ff65e69eca0019c34bf014c33
V Saravanan <saravanan.resume@gmail.com>
2019-09-11 16:41:15 +0800

    Removed footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import CategoryTree from './CategoryTree'
+import Header from './components/Header'
 
 class App extends Component {
   render() {
     return (
       <div class='wrapper'>
+        <header class='header'>
+          <Header />
+        </header>
         <aside class='aside aside-1'>
           <CategoryTree />
         </aside>
