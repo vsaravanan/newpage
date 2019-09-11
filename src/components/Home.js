8c4340fe59bfe022d6d47f689a98b18cbc525d7a
V Saravanan <saravanan.resume@gmail.com>
2019-09-11 17:10:05 +0800

    Making black color

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react'
-import CategoryTree from '../CategoryTree'
 import './style.css'
 
 class Home extends Component {
   render() {
-    return (
-      <div className='Home'>
-        <CategoryTree />
-      </div>
-    )
+    return <div className='Home'></div>
   }
 }
 
