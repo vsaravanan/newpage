e647ca882a410a2ac93678541f6ed12e10efa4a0
V Saravanan <saravanan.resume@gmail.com>
2019-09-11 16:40:56 +0800

    Removed footer

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
 import React, { Component } from 'react'
 import CategoryTree from './CategoryTree'
-import Header from './components/Header'
-import Footer from './components/Footer'
 
 class App extends Component {
-  state = { menu: false }
-  toggleMenu = () => {
-    this.setState({ menu: !this.state.menu })
-  }
   render() {
-    // const collapsed = this.state.menu ? '' : 'collapsed'
-
     return (
-      // <div>
-      //   <div id='sidebar' className={collapsed}>
-      //     <Home />
-      //   </div>
-      //   <div id='content'>
-      //     <Header />
-      //     <button className='menubutton' title='Toggle sidebar' onClick={this.toggleMenu}>
-      //       Menu
-      //     </button>
-      //     <h3>Some content</h3>
-      //     <Footer />
-      //   </div>
-      // </div>
-
       <div class='wrapper'>
-        <header class='header'>
-          <Header />
-        </header>
+        <aside class='aside aside-1'>
+          <CategoryTree />
+        </aside>
         <article class='main'>
           <p>
             Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis
@@ -38,13 +16,6 @@ class App extends Component {
             placerat eleifend leo.
           </p>
         </article>
-        <aside class='aside aside-1'>
-          <CategoryTree />
-        </aside>
-
-        <footer class='footer'>
-          <Footer />
-        </footer>
       </div>
     )
   }
