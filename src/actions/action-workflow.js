12e253b14d317cb4adefeabb7dd4fdc73a8cbe43
saravan <saravanan.resume@gmail.com>
2020-03-14 17:04:21 +0800

    removed anz related diagrams

--- src/actions/action-workflow.js
+++
@@ -1,6 +0,0 @@
-export const action_workflow = workflow => dispatch => {
-  dispatch({
-    type: 'workflow',
-    data: workflow,
-  })
-}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,16 +2,13 @@ import { combineReducers } from 'redux'
 import { connectRouter } from 'connected-react-router'
 import sidebar from './reducer-sidebar'
 import data_menus from './reducer-menus'
-import data_workflows from './reducer-workflows'
-import workflow from './reducer-workflow'
+
 
 const reducers = history =>
   combineReducers({
     router: connectRouter(history),
     sidebar,
     data_menus,
-    data_workflows,
-    workflow,
   })
 
 export default reducers
